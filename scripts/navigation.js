7ec7bc9361c8a69d088c18e02ed09ee9d3db36cd
sayhitoritu <rrai@byupathway.edu>
2026-01-07 20:01:40 +0530

    update

--- scripts/navigation.js
+++ scripts/navigation.js
@@ -2,5 +2,5 @@ const menuButton = document.getElementById("menu");
 const nav = document.getElementById("nav");
 
 menuButton.addEventListener("click", () => {
-    nav.style.display = nav.style.display === "flex" ? "none" : "flex";
+    nav.classList.toggle("open");
 });
